ef28bb7a2c778fbee91a2fbfe1ebda298fb6d79f
Aditya Sarnobat <sarnobatadi@gmail.com>
2021-11-20 19:38:07 +0530

    Amazon Price Scrapper

---
+++ backend/AmazonPriceTrack.py
@@ -0,0 +1,29 @@
+from selenium import  webdriver
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.common.exceptions import NoSuchElementException
+
+
+url = "https://www.amazon.in/Samsung-Galaxy-M12-Storage-Replacement/dp/B08XJG8MQM/ref=sr_1_1_sspa?keywords=mi%2Bphone&qid=1637417157&sr=8-1-spons&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUExRks4MFgwREhVUVdJJmVuY3J5cHRlZElkPUEwMzY5MTY1MzA2Vk05NDdYOFJCVyZlbmNyeXB0ZWRBZElkPUEwMDA3OTI2UjU2MUdKNUVHVEVPJndpZGdldE5hbWU9c3BfYXRmJmFjdGlvbj1jbGlja1JlZGlyZWN0JmRvTm90TG9nQ2xpY2s9dHJ1ZQ&th=1"
+priceList = []
+option = webdriver.ChromeOptions()
+option.add_argument('headless')
+driver = webdriver.Chrome(ChromeDriverManager().install(),options=option)
+
+def getAmazonPrice(url):
+    driver.get(url)
+    search_result = driver.find_element(By.XPATH, "//span[contains(@id,'productTitle')]")
+    try:
+        product_price = driver.find_element(By.XPATH, "//span[contains(@id,'priceblock_dealprice')]")
+    except NoSuchElementException:
+        product_price = driver.find_element(By.XPATH, "//span[contains(@id,'priceblock_ourprice')]")
+
+    print(search_result.text)
+    price = product_price.text.replace(u'\u20B9', '')
+    price = price.replace(',','')
+    price = (float(price))
+    print(price)
+    priceList.append(price)
+    print(priceList)
+
+getAmazonPrice(url)
